b2ae6275d132d31daed7082af83aeedb41746bec
arkashaqwe <valentinganicev061@gmail.com>
2025-07-13 19:12:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@
 # Настройки прокси
 PROXY = {
     'enable': True,
-    'type': 'socks5',
-    'host': '194.55.102.135',  # Ваш белорусский прокси
-    'port': 63001,
-    'username': 'GXjt8nK3',
-    'password': 'ghhPdd4C',
+    'type': 'socks5', # ИЛИ ДРУГОЙ ТИП
+    'host': '',
+    'port': ,
+    'username': '',
+    'password': '',
     'disable_webrtc': True,  # Блокировка WebRTC
     'force_dns': True  # Принудительный DNS через прокси
 }
 
 # Настройки сообщения
 MESSAGE = {
-    'text': '200 000 сум promo-kodi olingdir. Va uyga yetkazib berishga buyurtma bering! Birinchi yеtkazib berish bepul!!',
-    'apk_path': 'uzumtezkor.uz.apk'  # Путь к APK или None если не нужно
+    'text': '', # Ваш текст
+    'apk_path': ''  # Путь к APK
 }
 
-# Настройки Telegram
+# Настройки Telegram *RANDOM
 TELEGRAM = {
     'api_id': 2040,
     'api_hash': 'b18441a1ff607e10a989891a5462e627',
